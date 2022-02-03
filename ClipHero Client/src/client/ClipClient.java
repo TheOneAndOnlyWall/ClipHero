f9a19bda02bf3c68814a5d823551d6b037cc8789
Thomas <thomaswall@gmx.net>
2022-02-03 17:51:13 +0100

    Added Classes

--- ClipHero Client/src/client/ClipClient.java
+++ ClipHero Client/src/client/ClipClient.java
@@ -1,4 +1,15 @@
 package client;
 
-public class ClipClient {
+import netzklassen.Client;
+
+public class ClipClient extends Client {
+
+    public ClipClient(String pServerIP, int pServerPort) {
+        super(pServerIP, pServerPort);
+    }
+
+    @Override
+    public void processMessage(String pMessage) {
+
+    }
 }
