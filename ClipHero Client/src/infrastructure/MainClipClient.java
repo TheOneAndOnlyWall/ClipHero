d488f04f09a9daf24ccfe4ed7b91528b55d953cd
TheOneAndOnlyWall <94226683+TheOneAndOnlyWall@users.noreply.github.com>
2022-02-11 10:46:58 +0100

    Change: Replace Java Server finder with Kotlin

--- ClipHero Client/src/infrastructure/MainClipClient.java
+++ ClipHero Client/src/infrastructure/MainClipClient.java
@@ -1,22 +1,13 @@
 package infrastructure;
 
-import client.ServerFinder;
-
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import client.ServerFinderKotlin;
 
 public class MainClipClient {
 
     public static void main(String[] args) {
 
-        try {
-            ServerFinder finder = new ServerFinder();
-            finder.start();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+        ServerFinderKotlin finder = new ServerFinderKotlin();
+        finder.start();
 
     }
 
