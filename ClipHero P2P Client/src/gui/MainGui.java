67a17bb37f9316d7742f0fc9371267d9da85733a
Thomas <thomaswall@gmx.net>
2022-02-13 16:42:18 +0100

    Changed: Resource Bundle

--- ClipHero P2P Client/src/gui/MainGui.java
+++ ClipHero P2P Client/src/gui/MainGui.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,12 +10,21 @@ public class MainGui extends JFrame implements ActionListener {
     private JPanel cardPanel;
     private JPanel loginPanel;
     private JTextField textFieldMulticastIp;
+    private JButton connectButton;
+    private JCheckBox reconnectNextTimeCheckBox;
 
     public MainGui(){
-        this.setSize(300, 500);
+        super("ClipHero");
+        this.setSize(350, 500);
         this.getContentPane().add(mainPanel);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Set Location
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();
+        this.setLocation(width - this.getWidth(), height - this.getHeight() - 25);
     }
 
     @Override
