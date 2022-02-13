c075215619a1aeb41caea2b83b452950d2d2d5ff
TheOneAndOnlyWall <94226683+TheOneAndOnlyWall@users.noreply.github.com>
2022-02-13 12:04:52 +0100

    Initialized: Gui Interface GuiClipboardHandler

---
+++ ClipHero P2P Client/src/gui/GuiClipboardHandler.kt
@@ -0,0 +1,7 @@
+package gui
+
+interface GuiClipboardHandler {
+
+    fun processClipboardMessage(message: String)
+
+}
---
+++ ClipHero P2P Client/src/gui/MainGui.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainGui {
+    private JPanel mainPanel;
+    private JPanel cardPanel;
+    private JPanel loginPanel;
+    private JTextField textFieldMulticastIp;
+}
