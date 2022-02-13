b6c4e3e26bf05ca60f8b19a2336bd374481dcd4a
Thomas <thomaswall@gmx.net>
2022-02-13 12:34:23 +0100

    Added: Ressource Bundle Edit: initialized Gui

--- ClipHero P2P Client/src/gui/MainGui.java
+++ ClipHero P2P Client/src/gui/MainGui.java
@@ -1,10 +1,24 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainGui {
+public class MainGui extends JFrame implements ActionListener {
     private JPanel mainPanel;
     private JPanel cardPanel;
     private JPanel loginPanel;
     private JTextField textFieldMulticastIp;
+
+    public MainGui(){
+        this.setSize(300, 500);
+        this.getContentPane().add(mainPanel);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- ClipHero P2P Client/src/infrastructure/P2PMain.kt
+++ ClipHero P2P Client/src/infrastructure/P2PMain.kt
@@ -1,11 +1,13 @@
 package infrastructure
 
 import client.Client
+import gui.MainGui
 
 fun main(){
 
-    val client = Client()
-    client.start()
-    client.broadcastClipboard()
+//    val client = Client()
+//    client.start()
+//    client.broadcastClipboard()
 
+    val testGui = MainGui();
 }
